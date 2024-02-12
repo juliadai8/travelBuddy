68c56781b7a54f4a775f1ace7e970d5cbfc395e8
Adrian Solberg <adrianhsolberg@gmail.com>
2024-02-12 16:16:33 +0100

    Made parameters for DestinationBox
    Co-authored-by: @juliada
    Issue: #2

--- travel-buddy/src/components/DestinationBox.tsx
+++ travel-buddy/src/components/DestinationBox.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from 'react';
 import '../styles/DestinationBox.css';
 
+interface DestinationInterface {
+    country?: string;
+    city?: string;
+    rating?: string;
+    imgURL?: string;
+}
+
 // Note: The button must be alignes with the rating-stars when they are added
-const DestinationBox = () => {
+const DestinationBox: React.FC<DestinationInterface> = ({country, city, rating, imgURL}) => {
     return (
         <div className='box'>
             <div className='inner-div image-container'>
+                <img src={imgURL} alt="Error loading image" />
             </div>
             <div className='inner-div info-div'>
-                <h1>Title</h1>
-                <h2>Nation</h2>
+                <h1>{country}</h1>
+                <h2>{city}</h2>
             </div>
             <div className='inner-div more-div'>
                 <div className='rating-container'>
-                    Here comes rating
+                    {rating}
                 </div>
                 <div className="button-container">
                     <button>Read More</button>
