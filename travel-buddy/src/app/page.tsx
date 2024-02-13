e77d8d2e93e400402bc43d820c4ec98102d1fe4a
Julia Dai <juliada@stud.ntnu.no>
2024-02-13 21:13:52 +0100

    Remove unnecessary code and imports

--- travel-buddy/src/app/page.tsx
+++ travel-buddy/src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 import firebaseControl from './firebaseControl';
 import { useEffect } from 'react';
 import { getDocs } from 'firebase/firestore';
 import { DocumentData } from 'firebase/firestore';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 export default function Home() {
     const firebasecontroller = new firebaseControl;
@@ -21,11 +18,8 @@ export default function Home() {
         });
     }, []); */
 
-    ReactDOM.render(
-        <BrowserRouter>
-          <HomePage />
-        </BrowserRouter>,
-        document.getElementById('root'),
-      );
+    return(
+      <HomePage/>
+    )
 
 }
