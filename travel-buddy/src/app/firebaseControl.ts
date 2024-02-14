590203260b6db069086231ff2bb260a54f711e08
Julia Dai <juliada@stud.ntnu.no>
2024-02-14 09:18:08 +0100

    Create method to add destination to database

--- travel-buddy/src/app/firebaseControl.ts
+++ travel-buddy/src/app/firebaseControl.ts
@@ -2,7 +2,8 @@
 import { initializeApp } from "firebase/app";
 import {
   getFirestore, collection, getDocs,
-  addDoc
+  addDoc,
+  doc
 } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,6 +31,15 @@ class firebaseControl {
     return destinationsList;
   }
 
+  async addDestination(addCity: string, addCountry: string, addImgURL?: string, addCategory?: string[]) {
+    const docRef = collection(db, "destinations");
+    await addDoc(docRef, {
+      city: addCity,
+      country: addCountry,
+      imgUrl: addImgURL,
+      category: addCategory
+    });
+  }
 };
 
 export default firebaseControl;
