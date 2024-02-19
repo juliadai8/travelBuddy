3246d9dacc42a69d774c15937bbc0f4946122f02
Adrian Haabpiht Solberg <adriahso@stud.ntnu.no>
2024-02-19 12:55:12 +0100

    Remove unnecessary import

--- travel-buddy/src/components/AddDestination.tsx
+++ travel-buddy/src/components/AddDestination.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FC } from 'react';
+import React from 'react';
 import { useState } from 'react';
 import firebaseControl from "@/app/firebaseControl";
 import '../styles/AddDestination.css'
