11781096d94fd7a63c9173ceda158d7a8b74b1b4
Julia Dai <juliada@stud.ntnu.no>
2024-02-22 10:27:53 +0100

    Change to temporary firestore database

--- travel-buddy/src/app/firebaseControl.ts
+++ travel-buddy/src/app/firebaseControl.ts
@@ -11,12 +11,14 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWui1W0CBRAzvrMqHCOBUc3hMkmo3KkXw",
-  authDomain: "tdt4140-prosjekt.firebaseapp.com",
-  projectId: "tdt4140-prosjekt",
-  storageBucket: "tdt4140-prosjekt.appspot.com",
-  messagingSenderId: "807771308285",
-  appId: "1:807771308285:web:43ebb655b020317c987e94"
+  apiKey: "AIzaSyCj5CanDTH_T1opQ1OegZ1bypZUFIGxOfQ",
+  authDomain: "test-5c378.firebaseapp.com",
+  databaseURL: "https://test-5c378-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-5c378",
+  storageBucket: "test-5c378.appspot.com",
+  messagingSenderId: "453126250081",
+  appId: "1:453126250081:web:3a3ba8b1ee5477b2271420",
+  measurementId: "G-3V8H39KJPL"
 };
 
 // Initialize Firebase
