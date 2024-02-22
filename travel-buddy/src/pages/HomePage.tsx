a64d0e44e02574ee6e0080a3540ba4b0454a9717
Hallvard <hallvardfw@hotmail.no>
2024-02-22 10:05:59 +0100

    changed search to not search tags

--- travel-buddy/src/pages/HomePage.tsx
+++ travel-buddy/src/pages/HomePage.tsx
@@ -78,7 +78,7 @@ const HomePage = () => {
                 return true;
             }
             // If searchQuery is not empty, only return true for destinations that include the searchQuery in their category
-            return cityName.includes(searchQueryLowerCase) || countryName.includes(searchQueryLowerCase) || category.some(c => c.includes(searchQueryLowerCase));
+            return cityName.includes(searchQueryLowerCase) || countryName.includes(searchQueryLowerCase); //|| category.some(c => c.includes(searchQueryLowerCase));
         });
 }
     
