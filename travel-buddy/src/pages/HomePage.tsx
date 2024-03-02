c0967295fae190eb0d6762a3d31867e874d0d4dd
Julia Dai <juliada@stud.ntnu.no>
2024-03-02 11:57:47 +0100

    Remove access to editing when signing out

--- travel-buddy/src/pages/HomePage.tsx
+++ travel-buddy/src/pages/HomePage.tsx
@@ -48,6 +48,7 @@ const HomePage = () => {
                 setUser(undefined);
                 setOpenModal(false);
                 setOpenAddDestination(false);
+                setEdit(false);
             }
         });
         
