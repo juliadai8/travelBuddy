c6e7ff558e660b745740496be541ea58ea4534ae
Torjusbh <torjus@realfagsportalen.no>
2024-03-18 21:47:22 +0100

    Fixed bug in FilterAndSearchDestination Unit test

--- travel-buddy/src/tests/FilterAndSearchDestinations.test.tsx
+++ travel-buddy/src/tests/FilterAndSearchDestinations.test.tsx
@@ -1,19 +1,22 @@
 import '@testing-library/jest-dom'; 
 import filterDestinationsByType from '../components/FilterDestinations';
 import filteredDestinationsSearch from '../components/FilterDestinationBySearch';
+import { DocumentData } from 'firebase/firestore';
 
-interface Destination {
-    city: string;
-    country: string;
-    category: string[]; // Assuming category is an array of strings
-}
+// interface Destination {
+//     city: string;
+//     country: string;
+//     category: string[]; // Assuming category is an array of strings
+// }
 
 describe('filterDestinationsByType', () => {
-    const destinations = [
+    const destinations: DocumentData[] = [
         { city: 'Paris', country: 'France', category: ['City', 'Culture'] },
         { city: 'Rome', country: 'Italy', category: ['City', 'Historical'] },
         { city: 'Tokyo', country: 'Japan', category: ['City', 'Culture'] },
     ];
+
+    
 
     it('returns all destinations when no tags are provided', () => {
         const filteredDestinations = filterDestinationsByType(destinations);
@@ -33,34 +36,40 @@ describe('filterDestinationsByType', () => {
         const filteredDestinations = filterDestinationsByType(destinations, ['beach']);
         expect(filteredDestinations).toEqual([]);
     });
+
+    it('returns a single destinations by provided tags', () => {
+        const filteredDestinations = filterDestinationsByType(destinations, ['city', 'HIstorical']);
+        expect(filteredDestinations).toEqual([{ city: 'Rome', country: 'Italy', category: ['City', 'Historical'] }]);
+    });
 });
 
 describe('filteredDestinationsSearch', () => {
-    const destinations = [
+    const destinationsForSearch: DocumentData[] = [
         { city: 'Paris', country: 'France', category: ['City', 'Culture'] },
         { city: 'Rome', country: 'Italy', category: ['City', 'Historical'] },
         { city: 'Tokyo', country: 'Japan', category: ['City', 'Culture'] },
+        { city: 'Yukoshima', country: 'Japan', category: null },
     ];
 
     it('returns all destinations when search query is empty', () => {
-        const filteredDestinations = filteredDestinationsSearch(destinations, '');
-        expect(filteredDestinations).toEqual(destinations);
+        const filteredDestinations = filteredDestinationsSearch(destinationsForSearch, '');
+        expect(filteredDestinations).toEqual(destinationsForSearch);
     });
 
     it('filters destinations by search query', () => {
-        const filteredDestinations = filteredDestinationsSearch(destinations, 'Rome');
+        const filteredDestinations = filteredDestinationsSearch(destinationsForSearch, 'Rome');
         expect(filteredDestinations).toHaveLength(1);
         expect(filteredDestinations[0]).toEqual({ city: 'Rome', country: 'Italy', category: ['City', 'Historical'] });
     });
 
     it('filters destinations by search query case-insensitively', () => {
-        const filteredDestinations = filteredDestinationsSearch(destinations, 'tokyo');
+        const filteredDestinations = filteredDestinationsSearch(destinationsForSearch, 'tokyo');
         expect(filteredDestinations).toHaveLength(1); // Tokyo
         expect(filteredDestinations[0].city).toEqual('Tokyo');
     });
 
     it('returns empty array if no destinations match search query', () => {
-        const filteredDestinations = filteredDestinationsSearch(destinations, 'beach');
+        const filteredDestinations = filteredDestinationsSearch(destinationsForSearch, 'beach');
         expect(filteredDestinations).toEqual([]);
     });
 });
