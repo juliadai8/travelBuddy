b4748fcbe71d98a079b46c7119f25b60d7613c86
Adrian Solberg <adrianhsolberg@gmail.com>
2024-03-19 00:06:40 +0100

    Made DestinationBox test work with latest changes
    Issue: #38

--- travel-buddy/src/tests/DestinationBox.test.tsx
+++ travel-buddy/src/tests/DestinationBox.test.tsx
@@ -1,14 +1,16 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react'
+import { act, fireEvent, render, waitFor } from '@testing-library/react'
 import DestinationBox from '../components/DestinationBox';
+import { Rating } from '@mui/material';
 
+jest.mock('../components/WeatherDisplay');
 describe(DestinationBox, () => {
     it("renders without error when all props are valid", () => {
-        const { container } = render(<DestinationBox country='Country' city='City' rating='5' imgURL='https://media.snl.no/media/132105/article_topimage_Oslo_SNL.png' isLoggedIn={true}/>);        
+        const onReadMoreMock = jest.fn();
+        const { container } = render(<DestinationBox country='Country' city='City' rating={5} imgURL='https://media.snl.no/media/132105/article_topimage_Oslo_SNL.png' onReadMore={onReadMoreMock} isLoggedIn={true} user={undefined} id={'testId'}/>);        
         
         expect(container.querySelector('.box')).toBeInTheDocument();
 
-        
         const button = container.querySelector('button');
         expect(button).toBeInTheDocument();
         expect(button?.textContent).toEqual<string>('Read More');
@@ -21,12 +23,16 @@ describe(DestinationBox, () => {
         expect(country).toBeInTheDocument();
         expect(country?.textContent).toEqual<string>('Country');
 
-        const rating = container.querySelector('p');
-        expect(rating).toBeInTheDocument();
-        expect(rating?.textContent).toEqual<string>('5');
-
         const imgElement = container.querySelector('img');
         expect(imgElement?.getAttribute('src')).toEqual('https://media.snl.no/media/132105/article_topimage_Oslo_SNL.png');
         expect(imgElement?.getAttribute('alt')).toEqual('Error loading image');
+    });
+
+    it("allows clicking read more when logged in", async () => {
+        const onReadMoreMock = jest.fn();
+        const { getByRole } = render(<DestinationBox country='Country' city='City' rating={5} imgURL='https://media.snl.no/media/132105/article_topimage_Oslo_SNL.png' onReadMore={onReadMoreMock} isLoggedIn={true} user={undefined} id={'testId'}/>);        
+        const button = getByRole('button', {name: 'Read More'})
+        fireEvent.click(button);
+        expect(onReadMoreMock).toHaveBeenCalled();
     });
 });
