403fa3841c86a86f681709ac1df298f55a99e489
Adrian Haabpiht Solberg <adriahso@stud.ntnu.no>
2024-03-19 11:44:36 +0100

    review box does not show without review

--- travel-buddy/src/components/MyDestinationBox.tsx
+++ travel-buddy/src/components/MyDestinationBox.tsx
@@ -23,36 +23,31 @@ const MyDestinationBox: React.FC<DestinationInterface> = ({country, city, rating
 
     return (
         <div>
-        <div className='outer-box'>
-        <div className='box'>
-            <img src={imgURL} alt="Error loading image" className='inner-div' />
-            <div className='inner-div info-div'>
-                <h1>{city}</h1>
-                <h2>{country}</h2>
-            </div>
-            <div className='inner-div more-div'>
-                <div className='rating-container'>
-                <Rating name="half-rating" defaultValue={averageRating} precision={0.25} readOnly/>
+            <div className='outer-box' style={!myRating ? {marginBottom: '30px'} : {}}>
+                <div className='box'>
+                    <img src={imgURL} alt="Error loading image" className='inner-div' />
+                    <div className='inner-div info-div'>
+                        <h1>{city}</h1>
+                        <h2>{country}</h2>
+                    </div>
+                    <div className='inner-div more-div'>
+                        <div className='rating-container'>
+                        <Rating name="half-rating" defaultValue={averageRating} precision={0.25} readOnly/>
+                        </div>
+                        <div className='weather-container'>
+                            <WeatherDisplay country={country} city={city} />
+                        </div>
+                    </div>
                 </div>
-                <div className='weather-container'>
-                    <WeatherDisplay country={country} city={city} />
+            </div>
+            {myRating &&
+                <div className='review-container'>
+                    <h3>Your review for {city}</h3>
+                    <Rating name="half-rating" defaultValue={rating} precision={0.5} readOnly/>
+                    {review && <p>{review}</p>}
                 </div>
-                
-            </div>
-            
-            
+            } 
         </div>
-        </div>
-            
-            <div className='review-container'>
-                <h3>Your review for {city}</h3>
-                <Rating name="half-rating" defaultValue={rating} precision={0.5} readOnly/>
-                {review && <p>{review}</p>}
-            </div>
-            
-        </div>
-        
-        
     );
 }
 
